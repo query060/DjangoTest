0facc49b5c736d17fe4e2d983202638f2d5a59d9
query060 <jquery060@163.com>
2018-08-06 08:17:32 +0800

    2

--- atguigushop/apps/users/views.py
+++ atguigushop/apps/users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.backends import ModelBackend,get_user_model
+from django.contrib.auth.backends import ModelBackend, get_user_model
 from django.db.models import Q
 from rest_framework import viewsets
 from rest_framework import status
@@ -8,31 +8,29 @@ from .serializers import MSMSerializer
 from rest_framework import mixins
 from .models import VerifyCode
 
-
-
 # Create your views here.
 
-User=get_user_model()
+User = get_user_model()
 
-#短信验证,并且要把验证码保存到数据库
-class SmsCodeViewset(mixins.CreateModelMixin,viewsets.GenericViewSet):
+
+# 短信验证,并且要把验证码保存到数据库
+class SmsCodeViewset(mixins.CreateModelMixin, viewsets.GenericViewSet):
     # 序列化器，和表单类似有验证功能
     serializer_class = MSMSerializer
     queryset = VerifyCode.objects.all()
 
     def create(self, request, *args, **kwargs):
-        serializer=self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
-        headers=self.get_success_headers(serializer.data)
+        headers = self.get_success_headers(serializer.data)
 
-        return Response(serializer.data,status=status.HTTP_201_CREATED,headers=headers)
-
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
 
 class CustomModelBackend(ModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         try:
-            user=User.objects.get(Q(username=username)|Q(mobile=username))
+            user = User.objects.get(Q(username=username) | Q(mobile=username))
             if user.check_password(password):
                 return user
         except Exception as e:
